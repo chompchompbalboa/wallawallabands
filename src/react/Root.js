e828bafbab9a20d41b8a4e4be061c88e2f8d1bfd
Rocky Eastman Jr <eastmanrjr@gmail.com>
2018-01-10 21:07:15 -0800

    Initial commit

---
+++ src/react/Root.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+@connect()
+export default class Root extends Component {
+
+	constructor() {
+	}
+
+	componentDidMount() {
+	}
+
+  render () {
+    return (
+      <div>
+        Walla Walla Bands 2
+		  </div>
+    )
+  }
+}
